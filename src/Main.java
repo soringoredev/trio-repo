29bb8e7e7224efcfc575cd7da02484660bcd06b7
David Risneanu <davidrisneanu091gmail.com>
2026-07-08 18:43:09 +0300

    mesaj

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Text scris de Sorin");
+        System.out.println("Text scris de David");
 
     }
 }
